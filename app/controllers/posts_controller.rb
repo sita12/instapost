98f851a93e9f3ddfa62ea71b09e285bf3cc91623
sita12 <sitaneupane5700@gmail.com>
2017-08-25 16:09:08 +0545

    Setting up the routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+    def index
+    end    
+
+def create
+  @post = Post.find(params[:id])
+  @post.create(post_params)
+  redirect_to root_path
+end
+end
