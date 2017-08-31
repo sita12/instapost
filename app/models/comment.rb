b4e81918c0b213260c80e4b7803fa0f54229a1d0
sita12 <sitaneupane5700@gmail.com>
2017-08-31 23:15:42 +0545

    Posting Comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+belongs_to :post
+ validates :text, :user_id, :post_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
 validates :photo, :description, :user_id, presence: true
+has_many :comments
 end
