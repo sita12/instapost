0e1cfbdbea04129eb7c2fcb34e40600d759d1253
sita12 <sitaneupane5700@gmail.com>
2017-08-31 20:00:58 +0545

    Deleting Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
 before_action :authenticate_user!, only: [:new, :create]
-before_action :is_owner?, only: [:edit, :update]
+before_action :is_owner?, only: [:edit, :update, :destroy]
     def index
         @posts = Post.all.order('created_at DESC')
     end    
@@ -39,10 +39,16 @@ def update
 end
 private
 
-def is_owner?
+
  
 def is_owner?
   redirect_to root_path if Post.find(params[:id]).user != current_user
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to root_path
 end
 private
 
