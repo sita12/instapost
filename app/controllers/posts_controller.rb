c8010e36087ce63fda8b858808d402e1a861b25f
sita12 <sitaneupane5700@gmail.com>
2017-08-31 19:11:56 +0545

    Updating Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,18 @@ def create
     render :new, status: :unprocessable_entity
   end
 end
-
+def edit
+  @post = Post.find(params[:id])
+end
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  if @post.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
 private
 
 def post_params
