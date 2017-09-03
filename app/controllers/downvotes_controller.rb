ecfc30dbef462460c81ac1986c20c3f1256cf3b4
sita12 <sitaneupane5700@gmail.com>
2017-09-03 20:12:00 +0545

    Sending an AJAX Request

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -1,9 +1,13 @@
 class DownvotesController < ApplicationController
      before_action :authenticate_user!
 
-  def create
+ def create
     @post = Post.find(params[:post_id])
     @post.downvote_from current_user
-    redirect_to posts_path
+    
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.js {}
+    end
   end
 end
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,8 +2,12 @@ class UpvotesController < ApplicationController
     before_action :authenticate_user!
 
   def create
-     @post = Post.find(params[:post_id])
-     @post.liked_by current_user
-     redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    @post.liked_by current_user
+
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.js {}
+    end
   end
 end
